7653c3f6ddef4a830de940dca48b26aab2edfe8e
shreyanshVIT23 <179868648+shreyanshVIT23@users.noreply.github.com>
2025-05-09 21:33:10 +0530

    Explained Reasoning Behind it

--- solutions/day10/02_BulbSwitcher.py
+++ solutions/day10/02_BulbSwitcher.py
@@ -1,2 +1,11 @@
 # Title: 319. Bulb Switcher
 # URL: https://leetcode.com/problems/bulb-switcher/description/
+
+import math
+
+
+class Solution:
+    def bulbSwitch(self, n: int) -> int:
+        return int(math.sqrt(n))
+    
+        
